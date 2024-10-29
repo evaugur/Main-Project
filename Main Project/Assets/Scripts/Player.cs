28f88db8aadf485b21ffd8305702a6f17490f8a1
evaugur <evaaugur05@gmail.com>
2024-10-29 12:52:41 -0400

    Worked on triggers and scene spawning

--- Main Project/Assets/Scripts/Player.cs
+++ Main Project/Assets/Scripts/Player.cs
@@ -8,11 +8,34 @@ public class Player : MonoBehaviour
 
     [SerializeField] float runSpeed = 5f;
     private Rigidbody2D rb;
+    private float sceneSpawner = 0;
+
+    private Scene scene;
+
+    private void Awake()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        scene = SceneManager.GetActiveScene();
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (!string.Equals(scene.path, this.scene.path)) return;
+
+        rb = GetComponent<Rigidbody2D>();
+        Debug.Log(sceneSpawner.ToString());
+        Spawn();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -27,27 +50,52 @@ public class Player : MonoBehaviour
         rb.velocity = moveInput * runSpeed;
     }
 
+    void Spawn()
+    {
+        if (sceneSpawner == 1)
+        {
+            transform.position = new Vector3(2.99f, -3.93f, 0f);
+        }
+        if (sceneSpawner == 2)
+        {
+            transform.position = new Vector3(15.02f, -3.93f, 0f);
+        }
+        if (sceneSpawner == 3)
+        {
+            transform.position = new Vector3(-6.02f, -3.93f, 0f);
+        }
+        if(sceneSpawner == 4)
+        {
+            transform.position = new Vector3(9f, -3.93f, 0f);
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D trigger)
     {
         if (trigger.gameObject.tag == "Old Lady")
         {
             SceneManager.LoadScene("Old Lady's house");
+            sceneSpawner = 1;
         }
         if (trigger.gameObject.tag == "Kid")
         {
             SceneManager.LoadScene("Kid's house");
+            sceneSpawner = 2;
         }
         if (trigger.gameObject.tag == "Business Dad")
         {
             SceneManager.LoadScene("Business Dad's house");
+            sceneSpawner = 3;
         }
         if (trigger.gameObject.tag == "Gamer")
         {
             SceneManager.LoadScene("Gamer Bro's house");
+            sceneSpawner = 4;
         }
         if (trigger.gameObject.tag == "Neighborhood")
         {
             SceneManager.LoadScene("Neighborhood");
+            sceneSpawner = 0;
         }
     }
 
